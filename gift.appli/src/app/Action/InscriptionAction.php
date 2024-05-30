9cb7607a5f53eded6d959f1d1910e50525b52097
Flyguss <crafteur55100-verdun@hotmail.com>
2024-05-30 12:03:59 +0200

    Création des fichier permettant l'insciption rien n'est fonctionelle que graphique

---
+++ gift.appli/src/app/Action/InscriptionAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace gift\appli\app\Action;
+
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+class GetAcceuil extends AbstractAction {
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response{
+
+        $view = Twig::fromRequest($rq);
+        return $view->render($rs , 'TwigAcceuil.twig' );
+
+    }
+}
